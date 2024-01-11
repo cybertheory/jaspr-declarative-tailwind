b3d381167f9070df4e5868a8ba164815d01c326c
Rishabh Singh <rishabhsingh@berkeley.edu>
2024-01-11 13:57:26 -0500

    dependencies

--- examples/generator.dart
+++
@@ -1,9 +0,0 @@
-import '../tailwind.dart';
-import '../utils.dart';
-
-void main() {
-  print(TWGridColumns.span1.name);
-  List<String> classes = TWClassList(
-      generators: TWGenerators.layout(gridColumn: TWGridColumns.span1));
-  print(classes);
-}
